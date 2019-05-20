7e08c944dd688188a3b8cf4e22b6cc576b95b394
Melody Vincent <melodyvincent@Melodys-MacBook-Pro.local>
2019-05-20 14:13:48 -0600

    redux-1-afternoon-project

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import store, { ADD_INGREDIENTS} from './../../store';
 
 class Ingredients extends Component {
   constructor(props) {
     super(props);
+    const reduxState = store.getState();
     this.state = {
-      ingredients: [],
+      ingredients: reduxState.ingredients,
       input: ""
     };
   }
@@ -16,6 +18,10 @@ class Ingredients extends Component {
   }
   addIngredient() {
     // Send data to Redux state
+    store.dispatch({
+      type: ADD_INGREDIENTS,
+      payload: this.state.input
+    })
     this.setState({
       input: ""
     });
